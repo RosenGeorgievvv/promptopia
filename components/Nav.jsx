6c396f12d1ac10f1af52651203d24bbd3f7862f7
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-06-14 16:53:37 +0300

    Update Provider.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -78,6 +78,14 @@ const Nav = () => {
                                 <Link href="/create-prompt" className = "dropdown_link" onClick={() => setToggleDropdown(false)}>
                                     Create Prompt
                                 </Link>
+                                <button type="button" onClick={() =>{
+                                    setToggleDropdown(false);
+                                    signOut();
+                                }}
+                                className="mt-5 w-full black_btn"
+                                >
+                                    Sign Out
+                                </button>
                             </div>
                         )}
                     </div>
